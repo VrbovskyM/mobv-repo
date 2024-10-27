76aef5a5b84f6c1ad407041f6582fdd81e48f2ff
Michal Vrbovsky <100531156+VrbovskyM@users.noreply.github.com>
2024-10-27 19:24:23 +0100

    refactor: now working

--- app/src/main/java/com/example/mobv/fragments/FeedFragment.kt
+++ app/src/main/java/com/example/mobv/fragments/FeedFragment.kt
@@ -1,13 +1,13 @@
-package com.example.mobv
+package com.example.mobv.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.ImageView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.mobv.R
+import com.example.mobv.adapters.FeedAdapter
+import com.example.mobv.adapters.MyItem
 
 
 /**
@@ -26,16 +26,16 @@ class FeedFragment : Fragment(R.layout.fragment_feed) {
         recyclerView.adapter = feedAdapter
 
         feedAdapter.updateItems(listOf(
-            MyItem(0,R.drawable.zara1,"zara1"),
-            MyItem(1,R.drawable.zara2,"zara2"),
-            MyItem(2,R.drawable.zara3,"zara3"),
-            MyItem(3,R.drawable.zara4,"zara4"),
-            MyItem(4,R.drawable.zara5,"zara5"),
-            MyItem(5,R.drawable.zara6,"zara6"),
-            MyItem(6,R.drawable.zara7,"zara7"),
-            MyItem(7,R.drawable.zara8,"zara8"),
-            MyItem(8,R.drawable.zara9,"zara9"),
-            MyItem(9,R.drawable.zara10,"zara10"),
+            MyItem(0, R.drawable.zara1,"zara1"),
+            MyItem(1, R.drawable.zara2,"zara2"),
+            MyItem(2, R.drawable.zara3,"zara3"),
+            MyItem(3, R.drawable.zara4,"zara4"),
+            MyItem(4, R.drawable.zara5,"zara5"),
+            MyItem(5, R.drawable.zara6,"zara6"),
+            MyItem(6, R.drawable.zara7,"zara7"),
+            MyItem(7, R.drawable.zara8,"zara8"),
+            MyItem(8, R.drawable.zara9,"zara9"),
+            MyItem(9, R.drawable.zara10,"zara10"),
         ))
     }
 }
---
+++ app/src/main/java/com/example/mobv/fragments/LoginFragment.kt
@@ -0,0 +1,35 @@
+package com.example.mobv.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.example.mobv.R
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [LoginFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        view.findViewById<Button>(R.id.login_btn).setOnClickListener {
+            findNavController().navigate(R.id.action_LoginFragment_to_ProfileFragment)
+        }
+        // Inflate the layout for this fragment
+        return view
+    }
+}
